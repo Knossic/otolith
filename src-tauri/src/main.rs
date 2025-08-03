7e7a042958e7e23e1fbd728470f3499f45a52e2b
Kenneth Grauel <107715408+neocapy@users.noreply.github.com>
2025-08-03 10:17:00 -0400

    stubbed out watcher crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use watcher::hello_from_watcher;
+
 fn main() {
+    println!("{}", hello_from_watcher());
     otolith_lib::run()
 }
---
+++ watcher/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn hello_from_watcher() -> String {
+    "Hello from watcher".to_string()
+}
